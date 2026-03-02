28412ca845903de366d3f34942cac216603e9a73
16isaac89 <k.isaac1968@gmail.com>
2026-03-02 11:53:40 +0300

    first commit

---
+++ config/system-addons.php
@@ -0,0 +1,61 @@
+<?php return array (
+  'admin_panel' => 
+  array (
+    'active' => '1',
+    'username' => 'isaac',
+    'purchase_key' => 'ads',
+    'software_id' => 'MzE0NDg1OTc=',
+    'domain' => 'zayamart.test',
+    'software_type' => 'product',
+  ),
+  'vendor_panel' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'user_app' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'vendor_app' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'deliveryman_app' => 
+  array (
+    'active' => '1',
+    'name' => '',
+    'identifier' => '',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => 'MzYwODUwNDE=',
+    'domain' => '',
+    'software_type' => 'addon',
+    'errors' => 
+    array (
+    ),
+  ),
+  'theme_lifestyle' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+);
